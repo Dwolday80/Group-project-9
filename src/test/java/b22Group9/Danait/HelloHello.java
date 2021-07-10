f37eba7f278436b284f3fba3c5b3e6fa45869275
Dwolday <danaittewolde@yahoo.com>
2021-07-09 23:22:04 -0500

    my second commit

--- src/test/java/b22Group9/Danait/HelloHello.java
+++ src/test/java/b22Group9/Danait/HelloHello.java
@@ -3,5 +3,6 @@ package b22Group9.Danait;
 public class HelloHello {
     public static void main(String[] args) {
         System.out.println("hello everyone");
+        System.out.println("Murodil is the BEST!!!! So is Gurhan!!!");
     }
 }
